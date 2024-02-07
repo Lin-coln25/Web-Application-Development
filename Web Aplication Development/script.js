3a968eace31ffedccd450d11ed592f72327bdf7b
Masanga Lincoln <145362238+Lin-coln25@users.noreply.github.com>
2024-02-07 17:08:45 +0300

    Add files via upload

---
+++ Web Aplication Development/script.js
@@ -0,0 +1,15 @@
+let correctEmail='masangalincoln@gmail.com';
+let correctpassword='22u4491eve';
+
+let email=prompt("Enter your email");
+let password=prompt("Enter your password");
+
+function loginCredentials(enterEmail, enterPassword){
+    if (enterEmail===correctEmail && enterPassword===correctpassword){
+        console.log(`Your email: ${email}, login was sucessful!`);
+    }
+    else{
+        console.log(`Your email or password are invalid, login unsucessful`);
+    }
+}
+loginCredentials(email,password);
